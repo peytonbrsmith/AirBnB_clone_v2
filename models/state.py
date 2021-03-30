c94746819e063593587329245c96dea81d460b98
kwhit2 <kfw2@outlook.com>
2021-03-30 19:10:13 +0000

    Updated Class State to Inherit from Base as well

--- models/state.py
+++ models/state.py
@@ -2,7 +2,7 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column("name", String(128), nullable=False)
