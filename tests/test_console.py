cfd5dd0b6984bca193fa65f2cc968b6b324f35ce
kwhit2 <kfw2@outlook.com>
2021-03-31 22:51:49 +0000

    Added a test

--- tests/test_console.py
+++ tests/test_console.py
@@ -16,23 +16,29 @@ class TestConsole(unittest.TestCase):
     def test_all(self):
         """ test all method """
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("all NOPE")
+            HBNBCommand().onecmd("all NUGGET")
             self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
 
     def test_show(self):
         """ test show method """
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("show NOPE")
+            HBNBCommand().onecmd("show NUGGET")
             self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
 
     def test_create(self):
         """ test create method """
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("create NOPE")
+            HBNBCommand().onecmd("create NUGGET")
             self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
 
     def test_update(self):
         """ test update method """
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("update NOPE")
+            HBNBCommand().onecmd("update NUGGET")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
+
+    def test_destroy(self):
+        """ test destroy method """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("destroy NUGGET")
             self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
