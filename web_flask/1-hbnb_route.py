f17fb0c1578988db55e0da29df8f013d6f39132a
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-29 14:31:53 -0500

    added task 1 and 2

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Routes Hello
+"""
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world(strict_slashes=False):
+    """
+    Hello World
+
+    Returns:
+        [String] -- [Hello HBNB!]
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb(strict_slashes=False):
+    """
+    HBNB
+
+    Returns:
+        [String] -- [HBNB]
+    """
+    return 'HBNB'
+
+app.run(host='0.0.0.0')
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Routes Hello
+"""
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world(strict_slashes=False):
+    """
+    Hello World
+
+    Returns:
+        [String] -- [Hello HBNB!]
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb(strict_slashes=False):
+    """
+    HBNB
+
+    Returns:
+        [String] -- [HBNB]
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<path:subpath>')
+def cisfun(subpath, strict_slashes=False):
+    """
+    HBNB
+
+    Returns:
+        [String] -- [HBNB]
+    """
+    return 'C {}'.format(subpath.replace('_', ' '))
+
+
+app.run(host='0.0.0.0')
