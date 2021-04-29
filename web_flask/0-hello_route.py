22c3a7eeb28723edd2720647e40fcdb0ddd1cee6
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-29 19:00:37 +0000

    added task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Routes Hello
+"""
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world(strict_slashes=False):
+    """
+    Hello World
+
+    Returns:
+        [String] -- [Hello HBNB!]
+    """
+    return 'Hello HBNB!'
+
+app.run(host='0.0.0.0')
