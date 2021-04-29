f03be8131604dbb0f06bfc7d7f41c2a09df396d8
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-29 15:03:40 -0500

    added 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""
+Routes Hello
+"""
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world(strict_slashes=False):
+    """
+    Hello World
+
+    Returns:
+        [String] -- [Hello HBNB!]
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb(strict_slashes=False):
+    """
+    HBNB
+
+    Returns:
+        [String] -- [HBNB]
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<path:subpath>')
+def cisfun(subpath, strict_slashes=False):
+    """
+    HBNB
+
+    Returns:
+        [String] -- [HBNB]
+    """
+    return 'C {}'.format(subpath.replace('_', ' '))
+
+
+@app.route('/python/<path:subpath>')
+def pythonwpath(subpath, strict_slashes=False):
+    """
+    HBNB
+
+    Returns:
+        [String] -- [HBNB]
+    """
+    return 'Python {}'.format(subpath.replace('_', ' '))
+
+
+@app.route('/python/')
+def pythonwoutpath(strict_slashes=False):
+    """
+    HBNB
+
+    Returns:
+        [String] -- [HBNB]
+    """
+    return 'Python is cool'
+
+
+@app.route('/number/<int:number>')
+def number(number, strict_slashes=False):
+    """
+    HBNB
+
+    Returns:
+        [String] -- [HBNB]
+    """
+    return "{} is a number".format(number)
+
+
+app.run(host='0.0.0.0')
