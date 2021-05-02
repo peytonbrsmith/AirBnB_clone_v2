37cd9de9727d4d9360d663106b11f32a81c465da
peytonbrsmith <peytonbrsmith@gmail.com>
2021-05-02 18:24:31 -0500

    update

--- models/state.py
+++ models/state.py
@@ -2,22 +2,27 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
+import os
 import models
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade="delete", backref="state")
 
-    @property
-    def cities(self):
-        """ cities getter attribute """
-        cit_lis = []
-        for v in models.storage.all(City).values():
-            if v.state_id == self.id:
-                cit_lis.append(v)
-            return cit_lis
+    __tablename__ = "states"
+
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref='state', cascade='all, delete')
+    else:
+        name=""
+        @property
+        def cities(self):
+            """ Method that gets cities"""
+            cityList = []
+            for cityV in models.storage.all(City).values():
+                if getattr(cityV, "state_id") == self.id:
+                    cityList.append(cityV)
+            return(cityList)
