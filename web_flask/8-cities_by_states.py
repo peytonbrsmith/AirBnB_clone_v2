2fdb718ba867d1b2046f3d098cf19b320e9e8259
peytonbrsmith <peytonbrsmith@gmail.com>
2021-05-02 21:04:17 -0500

    added 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Routes Hello
+"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models import State
+from models import City
+
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states')
+def stateslist(strict_slashes=False):
+    """
+    HBNB
+
+    Returns:
+        [Template] -- [HBNB]
+    """
+    states = storage.all(State).values()
+    cities = storage.all(City).values()
+    return render_template('8-cities_by_states.html', states=states,
+                           cities=cities)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+        Closes Storage
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
